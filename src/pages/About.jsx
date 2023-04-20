337e886e229b5335df94438711ea622701c690c6
Amey <amey.k@research.iiit.ac.in>
2023-04-21 01:26:47 +0530

    bg ppt on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        boxSizing: "border-box",
+        alignItems: "center",
+        padding: "10px",
+        margin: "None",
+        height: "100vh",
+      }}
+    >
+      <img src="/1.png"></img>
+      <img src="/2.png"></img>
+      <img src="/3.png"></img>
+      <img src="/4.png"></img>
+      <img src="/5.png"></img>
+      <img src="/6.png"></img>
+      <img src="/7.png"></img>
+      <img src="/8.png"></img>  
+      <img src="/9.png"></img>
+      <img src="/10.png"></img>
+      <img src="/11.png"></img>
 
-export default About
+    </div>
+  );
+};
+
+export default About;
