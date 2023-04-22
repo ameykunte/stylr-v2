9b3e9774adff4bb40e404c246143faca947e8d15
Amey <amey.k@research.iiit.ac.in>
2023-04-22 09:17:38 +0530

    prepping for demo

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,17 +14,7 @@ const About = () => {
       }}
     >
       <iframe src="/Stylr.pdf" width="100%" height="800"></iframe>
-      {/* <img src="/1.png"></img>
-      <img src="/2.png"></img>
-      <img src="/3.png"></img>
-      <img src="/4.png"></img>
-      <img src="/5.png"></img>
-      <img src="/6.png"></img>
-      <img src="/7.png"></img>
-      <img src="/8.png"></img>  
-      <img src="/9.png"></img>
-      <img src="/10.png"></img>
-      <img src="/11.png"></img> */}
+
 
     </div>
   );
